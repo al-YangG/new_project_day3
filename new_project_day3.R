984d09be631deaf77f956dd196041e17b34f5d66
al-YangG <yangg@natur.cuni.cz>
2024-11-13 15:31:13 +0100

    add new messages

---
+++ new_project_day3.R
@@ -0,0 +1,22 @@
+## Day 3 -----
+library(usethis)
+
+# For existing project
+usethis::use_git()
+# Create new project with git tracking
+usethis::create_project("<DIRECTORY>")
+usethis::create_project("/Users/yanggang/Desktop/1/2024:2025/University/SPROuT/new_project_day3")
+# switch to the new project
+usethis::use_git()
+
+
+# Git clone - Copy (download) for remote repo to local machine
+usethis::create_from_github(
+  repo_spec = "https://github.com/CUNI-SPRouT/VersionControl-playground.git",
+  destdir = "/Users/yanggang/Desktop/1/2024:2025/University/SPROuT/new_project_day3",
+  fork = FALSE
+)
+
+
+
+utils::install.packages("httr2")
